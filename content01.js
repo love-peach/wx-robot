f8a49c4e5586d038c3e25a89f6510c875c92f8c7
张晋佩 <302784355@qq.com>
2018-05-23 14:59:59 +0800

    拆分逻辑

--- content01.js
+++ content01.js
@@ -23,7 +23,10 @@ var Msg = {
     ToUserName: "0",
     LocalID: "0",
     ClientMsgId: "0"
-}
+};
+
+var replyImgMediaId = '';
+var replyImgUrl = '';
 
 
 /**
@@ -31,23 +34,18 @@ var Msg = {
  *
  */
 chrome.extension.onRequest.addListener(function (request) {
-
-    //console.log(request);
-
-
     //监听最新的消息
     if (request.url.indexOf("cgi-bin/mmwebwx-bin/webwxsync") >= 0) {
-        console.log('接受消息也会触发?');
         //处理部分地域请求冲突
         if(request.url.indexOf("wx.qq.com")>=0){
             url_wx="https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsendmsg?"+"me=me";
         }
 
         if (!request.content) return;
-        news = JSON.parse(request.content);
-        if (news.AddMsgCount && news.AddMsgCount > 0 && wxInit != null && (news.AddMsgList[0].Content != '')) {
-            console.log('这个什么时候触发?????');
 
+        news = JSON.parse(request.content);
+
+        if (news.AddMsgCount && news.AddMsgCount > 0 && wxInit != null && (news.AddMsgList[0].Content != '')) {
             $.ajax({
                 async : false,
                 url: request.url + "&me=me",//避开循环标志
@@ -56,45 +54,11 @@ chrome.extension.onRequest.addListener(function (request) {
                 data: JSON.stringify(JSON.parse(request.params.postData.text)),
                 dataType: "json",
                 success: function (res) {
-                    console.log(res, 'res111');
+                    // 如果联系人有变化
                     if(res.ModContactCount !== 0) {
-                        Msg.FromUserName = wxInit.User.UserName;
-                        Msg.Content = 'hello, 扫码入群';
-                        res.ModContactList.forEach(function(item) {
-                            console.log(item, 'item');
-                            Msg.ToUserName = item.UserName;
-                            if(item.UserName.substr(0,2) !== '@@') {
-                                webwxsendmsg(BaseRequest, Msg, function () {
-                                    Msg.MediaId = "@crypt_31460533_f2e0008236c2674321984ac70b4f544a6bb3d76e64837af676c0e470b1095395f18a1a7c509f3ae14e72a4cda2c247e811800d440ba3c4cb41e48632d5c77c6478e3cc4bca61ed275f437b94b36f2aeffada690ee456bb70b64b88e760698f84f3ea4fbfdcd21a3df7551854ca3b6bd74f467dd2344e53cc600d0a1f4c6c235554eba5ee455aaab100faf480cabe7d07dbe6d41490790a64de92931ecb88a73941c17f3bdc6cb59ddb18e6a8726773aa0fb525b46f9d0a24c938635cfe074636a4beea72f75c3b550de09f5ef3a1b80582e797b29df2be6dde24d02b9b6a0af3c120fd3831d2ea7cc42b3cbaec7e34fb4dcc462193a763578b8a4949927a421b849bfab9f9df80c598da2ba939d622d1ba0714a0c28ef81ec363dc5bc63a8e401b7bb6cad0e6b2e669dfb4b38ea247dc367441652b658f44a489805df732efba";
-                                    webwxsendmsgimg(BaseRequest, Msg);
-                                });
-                            }
-                        });
+                        sayHelloToNewGuys(res);
                     } else {
-                        $.ajax({
-                            async : false,
-                            url: 'https://bird.ioliu.cn/mobile?shouji=' + res.AddMsgList[0].Content,//避开循环标志
-                            type: 'GET',
-                            dataType: "json",
-                            success: function (data) {
-                                console.log(data.msg, 'data2222');
-                                Msg.ToUserName = "filehelper";
-                                Msg.FromUserName = wxInit.User.UserName;
-                                if(data.status == '0'){
-                                    Msg.Content = '币8er:' + data.result.province + data.result.city + data.result.company;
-                                } else {
-                                    Msg.Content = "币8er：" + data.msg;
-                                }
-                                webwxsendmsg(BaseRequest, Msg)
-                            },
-                            error: function(err) {
-                                console.error(err, 'err222');
-                                Msg.Content = "币8er: 发送错误" ;
-                                Msg.ToUserName = "filehelper";
-                                Msg.FromUserName = wxInit.User.UserName;
-                                webwxsendmsg(BaseRequest, Msg)
-                            }
-                        })
+                        replyKeyWord(res);
                     }
                 },
                 error: function(err) {
@@ -112,15 +76,71 @@ chrome.extension.onRequest.addListener(function (request) {
     if (request.url.indexOf("cgi-bin/mmwebwx-bin/webwxbatchgetcontact") >= 0) {
         if (!request.Contact || !request.BaseRequest || !request.wxInit || !request.webwxbatchgetcontact) return;
 
+
         wxInit_a(request);
         Contact_a(request);
         webwxbatchgetcontact_a(request);
+        getReplyImgMediaId();
 
         BaseRequest = JSON.parse(request.BaseRequest.postData.text)
     }
-
-
 });
+
+// 给新添加的好友自动回复消息
+function sayHelloToNewGuys(res) {
+    Msg.FromUserName = wxInit.User.UserName;
+    res.ModContactList.forEach(function(item) {
+        Msg.ToUserName = item.UserName;
+        Msg.Content = 'hello '+ item.NickName +' 请扫码入群';
+        if(item.UserName.substr(0,2) !== '@@') {
+            webwxsendmsg(BaseRequest, Msg, function () {
+                Msg.MediaId = replyImgMediaId;
+                webwxsendmsgimg(BaseRequest, Msg);
+            });
+        }
+    });
+}
+
+// 请求关键字结果
+function replyKeyWord(res) {
+    $.ajax({
+        async : false,
+        url: 'https://bird.ioliu.cn/mobile?shouji=' + res.AddMsgList[0].Content,
+        type: 'GET',
+        dataType: "json",
+        success: function (data) {
+            Msg.ToUserName = "filehelper";
+            Msg.FromUserName = wxInit.User.UserName;
+            if(data.status == '0'){
+                Msg.Content = '币8er:' + data.result.province + data.result.city + data.result.company;
+            } else {
+                Msg.Content = "币8er：" + data.msg;
+            }
+            webwxsendmsg(BaseRequest, Msg)
+        },
+        error: function(err) {
+            console.error('请求关键字结果出错:', err);
+        }
+    })
+}
+
+// 获取图片 MediaId
+function getReplyImgMediaId() {
+    replyImgMediaId = '@crypt_31460533_f2e0008236c2674321984ac70b4f544a6bb3d76e64837af676c0e470b1095395f18a1a7c509f3ae14e72a4cda2c247e811800d440ba3c4cb41e48632d5c77c6478e3cc4bca61ed275f437b94b36f2aeffada690ee456bb70b64b88e760698f84f3ea4fbfdcd21a3df7551854ca3b6bd74f467dd2344e53cc600d0a1f4c6c235554eba5ee455aaab100faf480cabe7d07dbe6d41490790a64de92931ecb88a73941c17f3bdc6cb59ddb18e6a8726773aa0fb525b46f9d0a24c938635cfe074636a4beea72f75c3b550de09f5ef3a1b80582e797b29df2be6dde24d02b9b6a0af3c120fd3831d2ea7cc42b3cbaec7e34fb4dcc462193a763578b8a4949927a421b849bfab9f9df80c598da2ba939d622d1ba0714a0c28ef81ec363dc5bc63a8e401b7bb6cad0e6b2e669dfb4b38ea247dc367441652b658f44a489805df732efba';
+    return;
+    $.ajax({
+        async : false,
+        url: replyImgUrl,
+        type: 'GET',
+        dataType: "json",
+        success: function (data) {
+            replyImgMediaId = data
+        },
+        error: function(err) {
+            console.error('请求回复图片MediaId出错:', err);
+        }
+    })
+}
 
 
 /**
@@ -162,7 +182,7 @@ function webwxsendmsg(BaseRequest, Msg, cb) {
             if(cb) {
                 cb();
             }
-            console.log('发送成功 test');
+            console.log('发送成功 text');
         }
     })
 }
