245c0a51f84e7e6bd93a567d0be7a54d15951157
张晋佩 <302784355@qq.com>
2018-05-23 14:01:15 +0800

    修改自动回复图片

--- content01.js
+++ content01.js
@@ -13,7 +13,8 @@ var wxInit;
 //群联系
 var webwxbatchgetcontact;
 
-var url_wx="https://wx2.qq.com/cgi-bin/mmwebwx-bin/webwxsendmsg?"+"me=me";
+var url_wx="https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsendmsg?"+"me=me";
+var url_wx_img="https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsendmsgimg?"+"me=me"+"&fun=async"+"&f=json";
 
 var Msg = {
     Type: 1,
@@ -55,31 +56,46 @@ chrome.extension.onRequest.addListener(function (request) {
                 data: JSON.stringify(JSON.parse(request.params.postData.text)),
                 dataType: "json",
                 success: function (res) {
-                    console.log(res.AddMsgList[0].Content, 'res111');
-                    $.ajax({
-                        async : false,
-                        url: 'https://bird.ioliu.cn/mobile?shouji=' + res.AddMsgList[0].Content,//避开循环标志
-                        type: 'GET',
-                        dataType: "json",
-                        success: function (data) {
-                            console.log(data.msg, 'data2222');
-                            if(data.status == '0'){
-                                Msg.Content = "币8er：" + data.result.province + data.result.city + data.result.company;
-                            } else {
-                                Msg.Content = "币8er：" + data.msg;
+                    console.log(res, 'res111');
+                    if(res.ModContactCount !== 0) {
+                        Msg.FromUserName = wxInit.User.UserName;
+                        Msg.Content = 'hello, 扫码入群';
+                        res.ModContactList.forEach(function(item) {
+                            console.log(item, 'item');
+                            Msg.ToUserName = item.UserName;
+                            if(item.UserName.substr(0,2) !== '@@') {
+                                webwxsendmsg(BaseRequest, Msg, function () {
+                                    Msg.MediaId = "@crypt_31460533_f2e0008236c2674321984ac70b4f544a6bb3d76e64837af676c0e470b1095395f18a1a7c509f3ae14e72a4cda2c247e811800d440ba3c4cb41e48632d5c77c6478e3cc4bca61ed275f437b94b36f2aeffada690ee456bb70b64b88e760698f84f3ea4fbfdcd21a3df7551854ca3b6bd74f467dd2344e53cc600d0a1f4c6c235554eba5ee455aaab100faf480cabe7d07dbe6d41490790a64de92931ecb88a73941c17f3bdc6cb59ddb18e6a8726773aa0fb525b46f9d0a24c938635cfe074636a4beea72f75c3b550de09f5ef3a1b80582e797b29df2be6dde24d02b9b6a0af3c120fd3831d2ea7cc42b3cbaec7e34fb4dcc462193a763578b8a4949927a421b849bfab9f9df80c598da2ba939d622d1ba0714a0c28ef81ec363dc5bc63a8e401b7bb6cad0e6b2e669dfb4b38ea247dc367441652b658f44a489805df732efba";
+                                    webwxsendmsgimg(BaseRequest, Msg);
+                                });
                             }
-                            Msg.ToUserName = "filehelper";
-                            Msg.FromUserName = wxInit.User.UserName;
-                            webwxsendmsg(BaseRequest, Msg)
-                        },
-                        error: function(err) {
-                            console.error(err, 'err222');
-                            Msg.Content = "币8er: 发送错误" ;
-                            Msg.ToUserName = "filehelper";
-                            Msg.FromUserName = wxInit.User.UserName;
-                            webwxsendmsg(BaseRequest, Msg)
-                        }
-                    })
+                        });
+                    } else {
+                        $.ajax({
+                            async : false,
+                            url: 'https://bird.ioliu.cn/mobile?shouji=' + res.AddMsgList[0].Content,//避开循环标志
+                            type: 'GET',
+                            dataType: "json",
+                            success: function (data) {
+                                console.log(data.msg, 'data2222');
+                                Msg.ToUserName = "filehelper";
+                                Msg.FromUserName = wxInit.User.UserName;
+                                if(data.status == '0'){
+                                    Msg.Content = '币8er:' + data.result.province + data.result.city + data.result.company;
+                                } else {
+                                    Msg.Content = "币8er：" + data.msg;
+                                }
+                                webwxsendmsg(BaseRequest, Msg)
+                            },
+                            error: function(err) {
+                                console.error(err, 'err222');
+                                Msg.Content = "币8er: 发送错误" ;
+                                Msg.ToUserName = "filehelper";
+                                Msg.FromUserName = wxInit.User.UserName;
+                                webwxsendmsg(BaseRequest, Msg)
+                            }
+                        })
+                    }
                 },
                 error: function(err) {
                     console.error(err, 'err111');
@@ -124,12 +140,14 @@ chrome.extension.onRequest.addListener(function (request) {
  */
 
 var clientMsgId;
-function webwxsendmsg(BaseRequest, Msg) {
+function webwxsendmsg(BaseRequest, Msg, cb) {
 
     clientMsgId = new Date().getTime()
         + (Math.random() + "").substring(2, 6);
     Msg.LocalID = clientMsgId;
     Msg.ClientMsgId = clientMsgId;
+    Msg.Type = 1;
+    Msg.MediaId = '';
     $.ajax({
         url: url_wx,
         type: 'POST',
@@ -141,8 +159,35 @@ function webwxsendmsg(BaseRequest, Msg) {
         }),
         dataType: "json",
         success: function (data) {
-            // console.log(data)
-            console.log('发送成功');
+            if(cb) {
+                cb();
+            }
+            console.log('发送成功 test');
+        }
+    })
+}
+function webwxsendmsgimg(BaseRequest, Msg, cb) {
+    clientMsgId = new Date().getTime()
+        + (Math.random() + "").substring(2, 6);
+    Msg.LocalID = clientMsgId;
+    Msg.ClientMsgId = clientMsgId;
+    Msg.Type = 3;
+    Msg.Content = '';
+    $.ajax({
+        url: url_wx_img,
+        type: 'POST',
+        contentType: 'application/json;charset=UTF-8',
+        data: JSON.stringify({
+            BaseRequest: BaseRequest.BaseRequest,
+            Msg: Msg,
+            Scene: 0
+        }),
+        dataType: "json",
+        success: function (data) {
+            if(cb) {
+                cb();
+            }
+            console.log('发送成功 img');
         }
     })
 }
